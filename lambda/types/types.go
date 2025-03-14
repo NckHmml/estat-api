bcee71294adf84cfbb6becdb2a4f2517201e3cc2
Nick Hummel <NckHmml@users.noreply.github.com>
2025-03-14 15:59:49 +0100

    Fixed nullable int

--- lambda/types/types.go
+++ lambda/types/types.go
@@ -1,5 +1,7 @@
 package types
 
+import "database/sql"
+
 type Response struct {
 	Message string `json:"message"`
 }
@@ -10,16 +12,16 @@ type DBSecret struct {
 }
 
 type LabourParticipation struct {
-	Age                string `json:"age"`
-	AgeString          string `json:"age_string"`
-	Area               string `json:"area"`
-	AreaString         string `json:"area_string"`
-	LabourStatus       string `json:"labour_status"`
-	LabourStatusString string `json:"labour_status_string"`
-	Total              int    `json:"total"`
-	PersonOne          int    `json:"person_one"`
-	PersonTwo          int    `json:"person_two"`
-	PersonThree        int    `json:"person_three"`
-	PersonFour         int    `json:"person_four"`
-	PersonFive         int    `json:"person_five"`
+	Age                string        `json:"age"`
+	AgeString          string        `json:"age_string"`
+	Area               string        `json:"area"`
+	AreaString         string        `json:"area_string"`
+	LabourStatus       string        `json:"labour_status"`
+	LabourStatusString string        `json:"labour_status_string"`
+	Total              sql.NullInt32 `json:"total"`
+	PersonOne          sql.NullInt32 `json:"person_one"`
+	PersonTwo          sql.NullInt32 `json:"person_two"`
+	PersonThree        sql.NullInt32 `json:"person_three"`
+	PersonFour         sql.NullInt32 `json:"person_four"`
+	PersonFive         sql.NullInt32 `json:"person_five"`
 }
